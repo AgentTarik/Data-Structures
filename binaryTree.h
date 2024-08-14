f1bc41c24c1d386525b0b9f81e2e6594f5106092
Tarik <agenttarik99672@gmail.com>
2024-08-14 17:43:27 -0300

    delete node

--- binaryTree.h
+++ binaryTree.h
@@ -38,6 +38,49 @@ Node* insertNode(Node* root, int data) {
     return root;
 }
 
+Node* findMin(Node* root) {
+    while (root->left != NULL) {
+        root = root->left;
+    }
+    return root;
+}
+
+Node* maxNode(Node* root) {
+    while (root->right != NULL) {
+        root = root->right;
+    }
+    return root;
+}
+
+Node* deleteNode(Node* root, int data) {
+    if (root == NULL) {
+        return root;
+    }
+
+    if (data < root->data) {
+        root->left = deleteNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = deleteNode(root->right, data);
+    } else {
+        if (root->left == NULL) {
+            Node* temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+
+        Node* temp = findMin(root->right);
+
+        root->data = temp->data;
+
+        root->right = deleteNode(root->right, temp->data);
+    }
+    return root;
+}
+
 Node* findNode(Node* root, int data) {
     if (root == NULL) {
         printf("Node not found\n");
--- main.c
+++ main.c
@@ -3,15 +3,44 @@
 
 int main(void) {
 
-    Node* root = createNode(10);
-    insertNode(root, 2);
-    insertNode(root, 8);
-    insertNode(root, 11);
-    insertNode(root, 14);
-    insertNode(root, 15);
-    insertNode(root, 16);
+    struct Node* root = createNode(55);
+    root = insertNode(root, 50);
+    root = insertNode(root, 30);
+    root = insertNode(root, 20);
+    root = insertNode(root, 40);
+    root = insertNode(root, 70);
+    root = insertNode(root, 60);
+    root = insertNode(root, 80);
+    root = insertNode(root, 90);
+    root = insertNode(root, 100);
 
+    printf("In-order traversal before deletion: ");
     inOrder(root);
+    printf("\n");
+
+    root = deleteNode(root, 20);
+    printf("In-order traversal after deleting 20: ");
+    inOrder(root);
+    printf("\n");
+
+    root = deleteNode(root, 30);
+    printf("In-order traversal after deleting 30: ");
+    inOrder(root);
+    printf("\n");
+
+    root = deleteNode(root, 50);
+    printf("In-order traversal after deleting 50: ");
+    inOrder(root);
+    printf("\n");
+
+    root = deleteNode(root, 90);
+    printf("In-order traversal after deleting 90: ");
+    inOrder(root);
+    printf("\n");
+
+    printf("\n");
+
+    printf("%d", root->data);
 
     freeTree(root);
 
